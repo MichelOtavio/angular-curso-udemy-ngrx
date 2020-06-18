bd063f63787336ffe70ce04e01df7e11e4ff40db
Michel Otavio <studante2012@gmail.com>
2020-06-18 19:37:57 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { Module2Module } from './module2/module2.module';
+import { Module1Module } from './module1/module1.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    Module1Module,
+    Module2Module, 
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/module1/component2/component2.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Service1 } from '../service1.service';
+
+@Component({
+  selector: 'app-component2',
+  //providers: [Service1],
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.css']
+})
+export class Component2Component implements OnInit {
+
+  num = 0;
+
+  constructor (private service1: Service1) { }
+
+  ngOnInit() {
+    this.num =  this.service1.num;
+  }
+
+}
